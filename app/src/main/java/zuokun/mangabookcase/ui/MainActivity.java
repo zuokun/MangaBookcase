7bdb5a35dc8144bad41e76f352f5fe7a224bfd17
Zuokun <zeitiax@gmail.com>
2014-11-02 23:29:05 +0100

    Minor changed to display and added longclicklistener

--- app/src/main/java/zuokun/mangabookcase/ui/MainActivity.java
+++ app/src/main/java/zuokun/mangabookcase/ui/MainActivity.java
@@ -53,8 +53,6 @@ public class MainActivity extends Activity {
         // setting list adapter
         mangaListView.setAdapter(mangaListAdapter);
 
-
-        /*
         mangaListView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
@@ -65,7 +63,7 @@ public class MainActivity extends Activity {
                 return false;
             }
         });
-*/
+
     }
 
         private void prepareListData () {
